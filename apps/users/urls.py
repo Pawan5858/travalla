8f826a27112fa7c51bdf3ab9d5e210ff5e6003e9
Pawan5858 <101562678+Pawan5858@users.noreply.github.com>
2025-04-22 08:31:44 +0530

    Update urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,5 +3,6 @@ from .import views
 from .views import *
 
 urlpatterns = [
+    path('customers/',views.users,name='users'),
     path('users/<str:subRoute>/', usersAPIViewContoller.as_view(), name='users'),
 ]
