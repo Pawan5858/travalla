97e03bf8ac653b5139c259127f3746a424baea3f
Pawan5858 <101562678+Pawan5858@users.noreply.github.com>
2025-05-03 19:14:33 +0530

    Update urls.py

--- apps/guides/urls.py
+++ apps/guides/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('approvals/',views.approvals,name='approvals'),
     path('registerguides/',views.registered_guides,name='guides'),
+    path('guides/',views.guides,name='guides'),
     path('guides/<str:subRoute>/', guidesAPIViewContoller.as_view(), name='users')
 ]
