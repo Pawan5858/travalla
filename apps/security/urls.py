c869c0657cde70c1bde6356f63575dcf220ef35c
Pawan5858 <101562678+Pawan5858@users.noreply.github.com>
2025-05-03 19:22:38 +0530

    Update urls.py

--- apps/security/urls.py
+++ apps/security/urls.py
@@ -7,17 +7,5 @@ urlpatterns = [
     path('users/',views.users,name='users'),
     path('permissions/',views.permissions,name='permissions'),
     path('roles/',views.roles,name='roles'),
-
-
-
-
-    
     path('users_tabledata/', UsersTableDataView.as_view(), name='users_tabledata'),
-    
-    # api views
-    # path('logistic_operators/', LogisticOperatorsAPIView.as_view(), name='logistic_operator'),
-    # path('logistic_operators/<int:lgop_id>/', LogisticOperatorsAPIView.as_view(), name='logistic_operator_id'),
-    # path('logistic_operators_table_data/', LogisticOperatorsTableData.as_view(), name='logistic_operator_table_data'),
-    # path('vehicle-type-table-data/', VehicleTypeTableData.as_view(), name='vehicle_type_table_data'),
-    
 ]
